43bfc2cda9bec0c4211239476480fc5b09558dfc
Nicolás Toscano <nicolastoscano1@hotmail.com>
2025-05-19 14:39:50 -0500

    Creación del controlador Transaction y eliminación del controlador predeterminado

---
+++ TransactionsAPI/Controllers/TransactionController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TransactionsAPI.Data;
+
+namespace TransactionsAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly TransactionContext dbContext;
+
+        public TransactionController(TransactionContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllTransactions()
+        {
+            var allProducts = dbContext.Transactions.ToList();
+
+            return Ok(allProducts);
+        }
+
+
+    }
+}
