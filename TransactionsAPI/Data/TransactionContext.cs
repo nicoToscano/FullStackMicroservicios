89681f9551f9e180b70d76b4231aa600a730511d
Nicolás Toscano <nicolastoscano1@hotmail.com>
2025-05-19 14:40:12 -0500

    Creación del context para las transacciones

--- TransactionsAPI/Data/TransactionContext.cs
+++ TransactionsAPI/Data/TransactionContext.cs
@@ -1,6 +1,14 @@
-﻿namespace TransactionsAPI.Data
+﻿using Microsoft.EntityFrameworkCore;
+using TransactionsAPI.Models;
+
+namespace TransactionsAPI.Data
 {
-    public class TransactionContext
+    public class TransactionContext : DbContext
     {
+        public TransactionContext(DbContextOptions options) : base(options) 
+        {
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
     }
 }
