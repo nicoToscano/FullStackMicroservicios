f0ffafbbddfb32f721d4f2379a1105af10888a00
Nicolás Toscano <nicolastoscano1@hotmail.com>
2025-05-19 14:39:16 -0500

    Cambio de import using para la carpeta Data

--- ProductsAPI/Migrations/20250519045537_initial migration.Designer.cs
+++ ProductsAPI/Migrations/20250519045537_initial migration.Designer.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using ProductsAPI.Context;
+using ProductsAPI.Data;
 
 #nullable disable
 
--- ProductsAPI/Migrations/ProductsContextModelSnapshot.cs
+++ ProductsAPI/Migrations/ProductsContextModelSnapshot.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using ProductsAPI.Context;
+using ProductsAPI.Data;
 
 #nullable disable
 
