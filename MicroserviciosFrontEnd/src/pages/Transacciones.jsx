2a35d403c47f0ad162f16e99ad6ea736394dfc3e
Nicolás Toscano <nicolastoscano1@hotmail.com>
2025-05-20 14:15:59 -0500

    Mejorar la presentación de la vista de transacciones y corregir el título

--- MicroserviciosFrontEnd/src/pages/Transacciones.jsx
+++ MicroserviciosFrontEnd/src/pages/Transacciones.jsx
@@ -1,8 +1,62 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+
 export function Transacciones() {
+  const [transacciones, setTransacciones] = useState([]);
+
+  const getTransacciones = async () => {
+    try {
+      const response = await fetch("https://localhost:7295/api/transaction");
+      const data = await response.json();
+
+      if (response.status === 200) {
+        setTransacciones(data);
+
+        console.log("Transacciones:", data);
+      }
+    } catch (error) {
+      console.error("Error en la petición:", error);
+    }
+  };
+  useEffect(() => {
+    getTransacciones();
+  }, []);
 
   return (
     <>
-    <h1>transacciones</h1>
+      <h1>Transacciones</h1>
+      <div className="containerListaTransacciones">
+        {transacciones?.map((transaccion) => {
+            return (
+            <div className="listaTransacciones" key={transaccion.id}>
+              <ul>
+              <li className="date">
+                <strong>Fecha de transacción:</strong> {transaccion.dateOnly}
+              </li>
+              <li className="date">
+                <strong>Cantidad:</strong> {transaccion.cantidad}
+              </li>
+              <li className="date">
+                <strong>Precio total:</strong> {transaccion.precioTotal}
+              </li>
+              <li className="date">
+                <strong>Precio unitario:</strong> {transaccion.precioUnitario}
+              </li>
+              <li className="date">
+                <strong>Id del producto:</strong> {transaccion.productoId}
+              </li>
+              <li className="date">
+                <strong>Tipo de transacción:</strong> {transaccion.tipoDeTransaccion}
+              </li>
+              <li className="date">
+                <strong>Detalle:</strong> {transaccion.detalle}
+              </li>
+              </ul>
+            </div>
+            );
+        })}
+      </div>
     </>
   );
 }
