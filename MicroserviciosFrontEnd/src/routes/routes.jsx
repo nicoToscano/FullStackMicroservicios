51ad97c629372383b321550171bb4f95371eb745
Nicolás Toscano <nicolastoscano1@hotmail.com>
2025-05-20 11:19:28 -0500

    Actualización de rutas

--- MicroserviciosFrontEnd/src/routes/routes.jsx
+++ MicroserviciosFrontEnd/src/routes/routes.jsx
@@ -2,10 +2,11 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Inicio } from "../pages/Inicio";
 import { Productos } from "../pages/Productos";
 import { Transacciones } from "../pages/Transacciones";
-import { CreacionProductos } from "../pages/CreacionProductos";
-import { CreacionTransacciones } from "../pages/CreacionTransacciones";
-import { EdicionProductos } from "../pages/EdicionProductos";
-import { EdicionTransacciones } from "../pages/EdicionTransacciones";
+import { CrearTransaccion } from "../pages/CrearTransaccion";
+import { CrearProducto } from "../pages/CrearProducto";
+import { EditarProducto } from "../pages/EditarProducto";
+import { EditarTransaccion } from "../pages/EditarTransaccion";
+
 
 export function MyRoutes() {
   return (
@@ -14,15 +15,15 @@ export function MyRoutes() {
         <Route path="/" element={<Inicio/>} />
         <Route path="/productos" element={<Productos />} />
         <Route path="/transacciones" element={<Transacciones />} />
-        <Route path="/creacionproductos" element={<CreacionProductos />} />
+        <Route path="/creacionproductos" element={< CrearProducto/>} />
         <Route
           path="/creaciontransacciones"
-          element={<CreacionTransacciones />}
+          element={<CrearTransaccion />}
         />
-        <Route path="/edicionproductos/:idProducto" element={<EdicionProductos />} />
+        <Route path="/edicionproductos/:idProducto" element={<EditarProducto />} />
         <Route
           path="/ediciontransacciones"
-          element={<EdicionTransacciones />}
+          element={<EditarTransaccion />}
         />
       </Routes>
     </BrowserRouter>
