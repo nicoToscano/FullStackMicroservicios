7ccef28a011eef4dcbfd7fd201e315b4a84384df
Nicolás Toscano <nicolastoscano1@hotmail.com>
2025-05-20 10:04:17 -0500

    Actualizacion de estilos pagina inicio

--- MicroserviciosFrontEnd/src/pages/Inicio.jsx
+++ MicroserviciosFrontEnd/src/pages/Inicio.jsx
@@ -3,23 +3,25 @@ import { NavLink } from "react-router-dom";
 export function Inicio() {
   return (
     <>
-      <div className="logos">
-        <img className="logo net" src="./netcore.png" alt="Logo .NET" />
-        <img className="logo react" src="/react.svg" alt="Logo React" />
-        <img className="logo" src="/vite.svg" alt="Logo Vite" />
-      </div>
-      <h1>FullStack Microservicios .NET y React + Vite</h1>
-
-      <div className="container">
-        <div className="productos">
-          <NavLink to="/productos">
-            <h2 className="btnProductos">Lista de productos</h2>
-          </NavLink>
+      <div className="inicio">
+        <div className="logos">
+          <img className="logo net" src="./netcore.png" alt="Logo .NET" />
+          <img className="logo react" src="/react.svg" alt="Logo React" />
+          <img className="logo" src="/vite.svg" alt="Logo Vite" />
         </div>
-        <div className="transacciones">
-          <NavLink to="/transacciones">
-            <h2 className="btnTransacciones">Lista de transacciones</h2>
-          </NavLink>
+        <h1>FullStack Microservicios .NET y React + Vite</h1>
+
+        <div className="container">
+          <div className="productos">
+            <NavLink to="/productos">
+              <h2 className="btnProductos">Lista de productos</h2>
+            </NavLink>
+          </div>
+          <div className="transacciones">
+            <NavLink to="/transacciones">
+              <h2 className="btnTransacciones">Lista de transacciones</h2>
+            </NavLink>
+          </div>
         </div>
       </div>
     </>
